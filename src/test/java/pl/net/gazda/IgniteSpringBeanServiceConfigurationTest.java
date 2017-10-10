7546a15064a35203607b62dec26986c55d71fdb5
lgazda <lukasz@gazda.net.pl>
2017-10-10 15:31:48 +0200

    https://issues.apache.org/jira/browse/IGNITE-6555

--- src/test/java/pl/net/gazda/IgniteSpringBeanServiceConfigurationTest.java
+++ src/test/java/pl/net/gazda/IgniteSpringBeanServiceConfigurationTest.java
@@ -1,6 +1,7 @@
 package pl.net.gazda;
 
 import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteServices;
 import org.apache.ignite.IgniteSpringBean;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.resources.IgniteInstanceResource;
@@ -8,23 +9,24 @@ import org.apache.ignite.resources.SpringResource;
 import org.apache.ignite.services.Service;
 import org.apache.ignite.services.ServiceConfiguration;
 import org.apache.ignite.services.ServiceContext;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.List;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = IgniteSpringBeanServiceConfigurationTest.SpringBeansConfiguration.class)
 public class IgniteSpringBeanServiceConfigurationTest {
-    @Autowired
-    private AbstractApplicationContext context;
-
     @Test
     public void should_startContextAndInjectIgniteProxy() {
 
@@ -42,9 +44,10 @@ public class IgniteSpringBeanServiceConfigurationTest {
         }
 
         @Bean
-        public IgniteConfiguration igniteConfiguration(ServiceConfiguration serviceConfiguration) {
+        public IgniteConfiguration igniteConfiguration(ServiceConfiguration[] serviceConfigurations) {
             IgniteConfiguration configuration = new IgniteConfiguration();
-            configuration.setServiceConfiguration(serviceConfiguration);
+            //https://issues.apache.org/jira/browse/IGNITE-6555
+            //configuration.setServiceConfiguration(serviceConfigurations);
             return configuration;
         }
 
@@ -55,6 +58,33 @@ public class IgniteSpringBeanServiceConfigurationTest {
             configuration.setName(SimpleIgniteService.NAME);
             configuration.setMaxPerNodeCount(1);
             return configuration;
+        }
+
+        @Bean
+        public ServiceConfiguration igniteServiceConfiguration2(SimpleIgniteService igniteService) {
+            ServiceConfiguration configuration = new ServiceConfiguration();
+            configuration.setService(igniteService);
+            configuration.setName(SimpleIgniteService.NAME + "_2");
+            configuration.setMaxPerNodeCount(1);
+            return configuration;
+        }
+    }
+
+    //workaround for https://issues.apache.org/jira/browse/IGNITE-6555
+    @Component
+    public static class IgniteServiceDeploymentApplicationContextListener {
+        private final List<ServiceConfiguration> serviceConfigurations;
+
+        @Autowired
+        public IgniteServiceDeploymentApplicationContextListener(List<ServiceConfiguration> serviceConfigurations) {
+            this.serviceConfigurations = serviceConfigurations;
+        }
+
+        @EventListener({ContextRefreshedEvent.class})
+        void contextRefreshedEvent(ContextRefreshedEvent evt) {
+            Ignite ignite = evt.getApplicationContext().getBean(Ignite.class);
+            IgniteServices services = ignite.services();
+            serviceConfigurations.forEach(services::deploy);
         }
     }
 
@@ -80,12 +110,12 @@ public class IgniteSpringBeanServiceConfigurationTest {
 
     @Component
     public static class SimpleIgniteService implements Service {
-        public static final String NAME = "IgniteService";
+        private static final Logger LOG = Logger.getLogger(SimpleIgniteService.class);
+        static final String NAME = "IgniteService";
 
         @IgniteInstanceResource
         private transient Ignite ignite;
 
-        //@Autowired
         @SpringResource(resourceName = "springService")
         private transient SpringService springService;
 
@@ -96,7 +126,9 @@ public class IgniteSpringBeanServiceConfigurationTest {
         public void init(ServiceContext ctx) throws Exception {}
 
         @Override
-        public void execute(ServiceContext ctx) throws Exception {}
+        public void execute(ServiceContext ctx) throws Exception {
+            LOG.info("SERVICE DEPLOYED");
+        }
 
     }
 }
